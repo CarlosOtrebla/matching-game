51b2034828bbd95066d346f5cf92d412db2d94cc
CarlosOtrebla <carlosotrebla@gmail.com>
2022-10-25 23:02:41 -0300

    :construction: Create PlayerName components without css

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ import "./src/styles/settings/colors.css"
 import "./src/styles/generic/reset.css"
 import "./src/styles/elements/base.css"
 import BoardGame from './src/objetcs/BoardGame/index'
+import PlayerName from "./src/components/PlayerName"
+
 
 
 const $root = document.querySelector("#root")
-const $htmlBoardGame = BoardGame(6)
 
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame)
+
+$root.insertAdjacentHTML(
+    "beforeend", 
+`
+${PlayerName("Carlos")}
+${PlayerName("Sebastião")}
+${BoardGame(6)}
+`
+)
--- src/components/PlayerScore/index.js
+++ src/components/PlayerScore/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 function PlayerScore() {
     return`
     <article class="player-score">
-        <img src="images/Player1.png" alt="texto player 1">
+        
         
     </article>
     `
