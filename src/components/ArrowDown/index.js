d7c92515950b0f2fef83bcb83dd03ac056e441b8
CarlosOtrebla <carlosotrebla@gmail.com>
2022-11-04 11:44:58 -0300

    :lipstick: Add CSS in ArrowDown with animation

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,6 +1,11 @@
-function ArrowDown(){
+import "./style.css"
+
+function ArrowDown(currentPlayer=1){
     return/* html */`
-        <img src="images/ArrowDown.png" alt="Ícone de uma seta para baixo" />
+        <img class="arrow-down"
+        data-currentPlayer = ${currentPlayer}
+        src="images/ArrowDown.png" 
+        alt="Ícone de uma seta para baixo" />
     `
 }
 
